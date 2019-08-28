bfcbda62c3cf06a05d6830566da4650686b799c8
Ethan <1300157783@qq.com>
2019-08-28 20:58:28 +0800

    First commit.

---
+++ app/src/main/java/example/com/weatherapp/MainActivity.java
@@ -0,0 +1,15 @@
+package example.com.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+https://github.com/Ethan54188/weather-app.git
